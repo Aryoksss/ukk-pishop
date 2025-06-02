d0ff4dd1cfd430c06bfc3aa5de752f488bb93257
mirekan <softhard215@gmail.com>
2025-06-02 15:26:43 +0700

    feat: update carousel component and enhance dashboard layout with new images and sections

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,16 +1,98 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import AppLayout from '@/layouts/app-layout';
-
-// const breadcrumbs: BreadcrumbItem[] = [
-//     {
-//         title: 'Dashboard',
-//         href: '/dashboard',
-//     },
-// ];
 
 export default function Dashboard() {
     return (
         <AppLayout>
-            <p>Work In Progress</p>
+            <div className="flex flex-col items-center gap-4 p-4 px-16">
+                <Carousel className="w-full">
+                    <CarouselContent className="">
+                        <CarouselItem className="min-h-96 w-full bg-black">
+                            <img src="novapro.png" alt="" />
+                        </CarouselItem>
+                        <CarouselItem className="min-h-96 w-full bg-white">
+                            <img src="novapro.png" alt="" />
+                        </CarouselItem>
+                        <CarouselItem className="min-h-96 w-full bg-gray-200">
+                            <img src="novapro.png" alt="" />
+                        </CarouselItem>
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+                <div className="mt-12 grid grid-cols-5 gap-4">
+                    <div className="flex items-center justify-center rounded-sm bg-white shadow-md">
+                        <h2 className="p-4 text-2xl">Mouse</h2>
+                    </div>
+                    <div className="flex items-center justify-center rounded-sm bg-white px-16 py-4 shadow-md">
+                        <h2 className="p-4 text-2xl">Keyboard</h2>
+                    </div>
+                    <div className="flex items-center justify-center rounded-sm bg-white px-16 py-4 shadow-md">
+                        <h2 className="p-4 text-2xl">Deskmat</h2>
+                    </div>
+                    <div className="flex items-center justify-center rounded-sm bg-white px-16 py-4 shadow-md">
+                        <h2 className="p-4 text-2xl">Keycaps</h2>
+                    </div>
+                    <div className="flex items-center justify-center rounded-sm bg-white px-16 py-4 shadow-md">
+                        <h2 className="p-4 text-2xl">Switch</h2>
+                    </div>
+                </div>
+                <h2 className="mt-12 self-start text-2xl">Produk</h2>
+                <h3 className="self-start text-lg">Temukan produk terbaru kami</h3>
+                <div className="grid w-full grid-cols-5 gap-4 overflow-x-scroll">
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                </div>
+                <h3 className="self-start text-lg">Rekomendasi Produk</h3>
+                <div className="grid w-full grid-cols-5 gap-4 overflow-x-scroll">
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                </div>
+                <h3 className="self-start text-lg">Produk Terlaris</h3>
+                <div className="grid w-full grid-cols-5 gap-4 overflow-x-scroll">
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                    <div className="aspect-square rounded-md bg-white shadow-md">
+                        <img src="monoseries.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
         </AppLayout>
     );
 }
